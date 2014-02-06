4af370d9be761047fc2c7793ef19b12998f515b5
cjmills30 <cjmills30@tamu.edu>
2014-02-06 15:04:03 -0600

    Create DBMS_functions.cpp

---
+++ DBMS_functions.cpp
@@ -0,0 +1,8 @@
+
+createTable(string tabname, vector<string> cols, vector<string> primaryKeys);
+dropTable(string tabname);
+addRow(string tabname, vector<string> insertData);
+updateTable(string tabName, string varName, string newVal, string comparison);
+
+
+
