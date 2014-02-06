d5cdedc44b9792248493cab98814020716bef7df
cjmills30 <cjmills30@tamu.edu>
2014-02-06 15:06:07 -0600

    Update DBMS_functions.cpp

--- DBMS_functions.cpp
+++ DBMS_functions.cpp
@@ -1,8 +1,12 @@
-
 createTable(string tabname, vector<string> cols, vector<string> primaryKeys);
 dropTable(string tabname);
 addRow(string tabname, vector<string> insertData);
 updateTable(string tabName, string varName, string newVal, string comparison);
+deleteFrom(string tabname);
+selectElem(T, T);
+displayTable(string tabname);
+insertRowsWithQualification(string relation);
+
 
 
 
