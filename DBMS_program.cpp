3be9429eb24beaf2ef157c49084ea857abcaa0d0
Alyse <alyseshan@neo.tamu.edu>
2014-02-10 18:14:45 -0600

    Update DBMS_program.cpp

--- DBMS_program.cpp
+++ DBMS_program.cpp
@@ -18,8 +18,9 @@ public:
 	string& operator[] (int i) {
 		return data[i];
 	}
-	//goes through a column and deletes an element
-	void deleteCell(int i){
+	//goes through a column and deletes a cell (element)
+	void deleteCell(int i)
+	{
 		vector<string>::iterator itt = data.begin();
 		for(; itt < data.end(); itt++)
 		{
@@ -72,8 +73,9 @@ public:
 		}
 		return out << returnOutput;
 	}
-	//delete function
-	void deleteFrom(string tabname, string comparison){
+	// DELETE FROM function
+	void deleteFrom(string tabname, string comparison)
+	{
 		for(size_t i = 0; i<columns[0].getSize(); ++i)  // goes through first column of table
 		{
 			if(columns[0][i] == comparison)  // finds index of the data that matches comparison
